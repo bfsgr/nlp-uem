7ec5e04c27668301a82e97a3be95b3108cef850f
bfsgr <bfsgr.code@protonmail.com>
2024-03-03 16:32:36 -0300

    adiciona arquivo para download de dados

---
+++ download.py
@@ -0,0 +1,12 @@
+import nltk
+
+
+def main():
+    nltk.download('punkt')
+    nltk.download('stopwords')
+    nltk.download('averaged_perceptron_tagger')
+    nltk.download('wordnet')
+
+
+if __name__ == "__main__":
+    main()
--- main.py
+++ main.py
@@ -562,11 +562,6 @@ def process_file(fullpath: str) -> ScyPaper:
 
 
 def main():
-    nltk.download('punkt', quiet=True)
-    nltk.download('stopwords', quiet=True)
-    nltk.download('averaged_perceptron_tagger', quiet=True)
-    nltk.download('wordnet', quiet=True)
-
     parser = argparse.ArgumentParser(
         prog='Paper Analyzer')
 
